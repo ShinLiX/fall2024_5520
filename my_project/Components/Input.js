9d5be75667a6e9a67e00bf26c8e733e576cc6411
ShinLiX <xli20010110@gmail.com>
2024-09-12 23:54:31 -0700

    Q3 & Q4 completed

--- my_project/Components/Input.js
+++ my_project/Components/Input.js
@@ -4,6 +4,9 @@ import React, {useState, useEffect, useRef} from 'react';
 export default function Input({ autoFocus}) {
   const [text, setText] = useState("");
   const inputRef = useRef(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [message, setMessage] = useState("");
+
 
   useEffect(() => {
     if (autoFocus && inputRef.current) {
@@ -15,6 +18,19 @@ export default function Input({ autoFocus}) {
     setText(changedText);
   }
 
+  function handleBlur() {
+    setIsFocused(false);
+    if (text.length >= 3) {
+      setMessage("Thank you");
+    } else {
+      setMessage("Please type more tham 3 characters");
+    }
+  }
+
+  function handleFocus() {
+    setIsFocused(true);
+    setMessage("");
+  }
 
   return (
     <View>
@@ -24,11 +40,16 @@ export default function Input({ autoFocus}) {
         style={{borderBottomColor: "purple", borderBottomWidth:2}} 
         value={{text}} 
         onchangeText={updateText}
+        ref={inputRef}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
       />
       {text.length > 0 && (
         <Text>Character Count: {text.length}</Text>
       )}
-      {}
+      {!isFocused && message.length > 0 && (
+        <Text>{message}</Text>
+      )}
     </View>
     );
 }
