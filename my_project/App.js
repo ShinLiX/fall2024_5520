5a3f1b67f1147d5f769f0cba8135ca1cf27ae652
ShinLiX <xli20010110@gmail.com>
2024-09-19 12:48:30 -0700

    app changed

--- my_project/App.js
+++ my_project/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import Header from './Components/Header';
 import { useState } from 'react';
 import Input from './Components/Input';
 
 export default function App() {
   const [receivedData, setReceivedData] = useState(""); // Store the input data
+  const [isModelVisible, setIsModelVisible] = useState(false); // For Button (not used yet)
   const appName = "My app";
   
   function handleInputData(data) {
@@ -16,13 +17,19 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <StatusBar style='auto' />
-      <Header name={appName} />
-      <Input style={styles.inputStyle} autoFocus={true} inputHandler={handleInputData} />
-      {/* Display the received data */}
-      {receivedData.length > 0 && (
-        <Text>Received Data: {receivedData}</Text>
-      )}
+      <View style={styles.topView}>
+        <StatusBar style='auto' />
+        <Header name={appName} />
+        <Button
+          title="Add a Goal"
+          onPress={() => setIsModelVisible(true)} />
+      </View>
+      <View style={styles.bottomView}>
+        <Input style={styles.inputStyle} autoFocus={true} inputHandler={handleInputData} />
+        {receivedData.length > 0 && (
+          <Text style={styles.receivedDataText}>Received Data: {receivedData}</Text>
+        )}
+      </View>
     </View>
   );
 }
@@ -31,13 +38,30 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  topView: {
+    flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bottomView: {
+    flex: 4,
+    backgroundColor: 'purple',  // Set background color of the bottom view
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%', // Make sure the view covers the entire width
   },
   inputStyle: {
     borderColor: 'blue',
     borderWidth: 1,
     padding: 10,
     margin: 10,
+    backgroundColor: '#fff', // To make sure input field has a white background
+    width: '80%',  // To make the input box more reasonable in size
+  },
+  receivedDataText: {
+    color: '#fff', // Text color for the received data in bottom view
+    marginTop: 10,
   },
 });
