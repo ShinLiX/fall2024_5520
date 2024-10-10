cd0e04bec82b6a3245b72403ea4f1866ca784adc
ShinLiX <xli20010110@gmail.com>
2024-10-10 11:15:40 -0700

    Wrapped goalitem in a pressable so we could navigate to goalDetails by tapping on it

--- my_project/Components/GoalItem.js
+++ my_project/Components/GoalItem.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react'
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Button, Pressable } from 'react-native'
 
 export default function GoalItem ({goalItem, handleDelete, detailHandler}) {
 
   return (
     <View style={styles.scrollView}>
+      <Pressable style={styles.horizontalContainer} onPress={()=>{detailHandler(goalItem)}}>
       <Text style={styles.text}>{goalItem.text}</Text>
       <Button title="X" onPress={()=>{handleDelete(goalItem.id)}} color="grey"/>
-      <Button title="i" onPress={()=>{detailHandler(goalItem)}} color="grey"/>
+      {/* <Button title="i" onPress={()=>{detailHandler(goalItem)}} color="grey"/> */}
+      </Pressable>
     </View>
   );
 }
@@ -24,5 +26,9 @@ const styles = StyleSheet.create({
     //marginVertical: 5,
     fontSize: 20,
     padding: 5,
+  },
+  horizontalContainer: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
